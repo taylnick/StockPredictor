2d54ab0156460ed69bb70108bd87e6aa5dca061a
hedgtyle <hedgtyle@isu.edu>
2020-11-09 17:32:24 -0700

    implemented new datamatrix structure with linear regression

--- main.py
+++ main.py
@@ -2,19 +2,51 @@ import os
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsRegressor
-import tensorflow as tf
-from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import sklearn.metrics as met
+# import tensorflow as tf
+# from tensorflow import keras
 
 data_dir = 'Data/Stocks'
 all_files = os.listdir(data_dir)
 
-ratio_requested = .1
-num_requested = int(len(all_files) * ratio_requested)
+def get_stock_subset_ratio(ratio):
+    num_requested = int(len(all_files) * ratio)
+    return np.random.choice(all_files, num_requested)
+def get_stock_subset_num(num):
+    return np.random.choice(all_files, num)
+def build_data_matrix(file, window_size):
+    text_file = open(data_dir + '/' + file, 'r')
+    lines = text_file.readlines()
 
-subset = np.random.choice(all_files, num_requested)
+    # remove newlines https://stackoverflow.com/questions/43447221/removing-all-spaces-in-text-file-with-python-3-x
+    lines = [line.replace('\n', '') for line in lines]
 
-# print('Selected subset of ' + str(len(subset)) + ' files out of ' + str(len(all_files)))
-# print(subset)
+    # must leave [window_size] entries to build row
+    max_window_start = (len(lines) - 1) - window_size
+
+    features = []
+    labels = []
+
+    for x in range(1, max_window_start):
+
+        row = []
+        for j in range(x, x + window_size):
+            split_line = lines[j].split(',')
+            # Date,Open,High,Low,Close,Volume,OpenInt
+            # 0    1    2    3   4     5      6
+            row.append(float(split_line[1]))
+            row.append(float(split_line[2]))
+            row.append(float(split_line[3]))
+            row.append(float(split_line[4]))
+            row.append(int(split_line[5]))
+        features.append(row)
+
+        # target is the closing price of the next day
+        labels.append(float(lines[x + window_size].split(',')[4]))
+
+    return features, labels
 
 def print_subset(subset):
     for i, file in enumerate(subset):
@@ -35,101 +67,46 @@ def print_subset(subset):
                 print(line)
             else:
                 break
-
-def getX(file):
-
+def print_file(file, num_lines):
     text_file = open(data_dir + '/' + file, 'r')
     print(file)
-    lines = text_file.readlines()
 
-    X = [None] * (len(lines) - 1)
+    lines = text_file.readlines()
 
     # remove newlines https://stackoverflow.com/questions/43447221/removing-all-spaces-in-text-file-with-python-3-x
     lines = [line.replace('\n', '') for line in lines]
 
-    for j, line in enumerate(lines):
-        if j == 0:
-            continue
+    if (num_lines < 0):
+        # print all
+        for x in range(1, len(lines)):
+            print(lines[x])
+    else:
+        # print first [num_lines] lines
+        for x in range(1, num_lines + 1):
+            print(lines[x])
+        print('...')
+def print_data(X, y):
+    for x in range(len(X)):
+        print(','.join(map(str, X[x])) + ' -> ' + y[x])
 
-        split_line = line.split(',')
+def linear_regression_predict(x, y):
+    # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
+    lin_reg = LinearRegression().fit(X_train, y_train)
+    return lin_reg.predict(X_test)
 
-        dat = [None] * 5
-
-        # Date,Open,High,Low,Close,Volume,OpenInt
-        # 0    1    2    3   4     5      6
-
-        # dat[0] = split_line[0]
-        dat[0] = float(split_line[1])
-        dat[1] = float(split_line[2])
-        dat[2] = float(split_line[3])
-        dat[3] = float(split_line[4])
-        dat[4] = int(split_line[5])
-        # dat[6] = int(split_line[6])
-
-        X[j-1] = dat
-        print(", ".join(map(str, dat)))
-
-    return X
-
-def buildDataMatrix(file, window_size):
-    text_file = open(data_dir + '/' + file, 'r')
+files = get_stock_subset_num(5)
+for file in files:
     print(file)
-    lines = text_file.readlines()
+    window_size = 5
 
-    X = [None] * (len(lines) - 1)
+    # print_file(file, 10)
+    X, y = build_data_matrix(file, window_size)
 
-    # remove newlines https://stackoverflow.com/questions/43447221/removing-all-spaces-in-text-file-with-python-3-x
-    lines = [line.replace('\n', '') for line in lines]
+    # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-    for j, line in enumerate(lines):
-        # first line is just column labels
-        if j == 0:
-            continue
+    y_pred = linear_regression_predict(X_test, y_test)
 
-        split_line = line.split(',')
+    mse = met.mean_squared_error(y_test, y_pred)
 
-        dat = [None] * 5
-
-        # Date,Open,High,Low,Close,Volume,OpenInt
-        # 0    1    2    3   4     5      6
-
-        # dat[0] = split_line[0]
-        dat[0] = float(split_line[1])
-        dat[1] = float(split_line[2])
-        dat[2] = float(split_line[3])
-        dat[3] = float(split_line[4])
-        dat[4] = int(split_line[5])
-        # dat[6] = int(split_line[6])
-
-        X[j - 1] = dat
-        print(", ".join(map(str, dat)))
-
-    return X
-
-
-test_file = subset[0]
-X = getX(test_file)
-
-window_size = 25
-knn = KNeighborsRegressor(n_neighbors=7)
-
-# iterate this
-window_start = 0
-window_end = window_start + window_size
-
-samples = X[window_start:window_end]
-
-y = X[window_end + 1:window_end +1 + window_size]
-
-knn.fit(samples, y)
-
-X_test = X[window_start + 1:window_end + 1]
-prediction = knn.predict(X_test)
-y_real = X[window_end + 1]
-
-print("Input: ")
-for x in X_test:
-    print(",".join(map(str,x)))
-
-print("Output: \n" + ",".join(map(str,prediction[-1])))
-print("Actual: \n" + ",".join(map(str, y_real)))
+    print("MSE: " + str(mse) + "\n")
