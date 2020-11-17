d94442fda129a9f1411432352c99dedbc0de241e
taylnick <taylnick@isu.edu>
2020-11-17 11:25:19 -0700

    Changes to display the plot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 import pandas as pd
+import pydot
+import graphviz
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
@@ -9,16 +11,26 @@ import sklearn.metrics as met
 import tensorflow as tf
 from tensorflow import keras
 from sklearn.preprocessing import normalize
+from random import seed
+from random import random
+from sklearn.model_selection import learning_curve
+import matplotlib.pyplot as plt
 
+# data_dir = 'drive/MyDrive/SampleStockData'
 data_dir = 'Data/Stocks'
 all_files = os.listdir(data_dir)
 np.random.seed(43)
 
+
 def get_stock_subset_ratio(ratio):
     num_requested = int(len(all_files) * ratio)
     return np.random.choice(all_files, num_requested)
+
+
 def get_stock_subset_num(num):
     return np.random.choice(all_files, num)
+
+
 def build_data_matrix(file, window_size):
     text_file = open(data_dir + '/' + file, 'r')
     lines = text_file.readlines()
@@ -50,6 +62,8 @@ def build_data_matrix(file, window_size):
         labels.append(float(lines[x + window_size].split(',')[4]))
 
     return features, labels
+
+
 def build_np_matrix(file, window_size):
     text_file = open(data_dir + '/' + file, 'r')
     lines = text_file.readlines()
@@ -89,8 +103,8 @@ def build_np_matrix(file, window_size):
         percent_increase = difference / prev_day_close
         labels.append(percent_increase)
 
-
     return np.array(features), np.array(labels)
+
 
 def print_subset(subset):
     for i, file in enumerate(subset):
@@ -111,6 +125,8 @@ def print_subset(subset):
                 print(line)
             else:
                 break
+
+
 def print_file(file, num_lines):
     text_file = open(data_dir + '/' + file, 'r')
     print(file)
@@ -129,23 +145,28 @@ def print_file(file, num_lines):
         for x in range(1, num_lines + 1):
             print(lines[x])
         print('...')
+
+
 def print_data(X, y):
     for x in range(len(X)):
         print(','.join(map(str, X[x])) + ' -> ' + str(y[x]))
 
+
 # these are broken
 def build_model(num_hidden_layers=2, num_neurons=100, output_neurons=2, learning_rate=3e-3,
-                input_shape=(80, 80, 3)):
-  model = keras.models.Sequential()
-  model.add(keras.layers.InputLayer(input_shape=input_shape))
-  model.add(keras.layers.Flatten(input_shape=(input_shape)))
-  for layer in range(num_hidden_layers):
-    model.add(keras.layers.Dense(num_neurons, activation='relu'))
-  model.add(keras.layers.Dense(output_neurons, activation='softmax'))
-  model.compile(loss='sparse_categorical_crossentropy',
-                optimizer=keras.optimizers.SGD(lr=learning_rate),
-                metrics=['accuracy'])
-  return model
+                input_shape=(25)):
+    model = keras.models.Sequential()
+    model.add(keras.layers.InputLayer(input_shape=input_shape))
+    # model.add(keras.layers.Flatten(input_shape=(input_shape)))
+    for layer in range(num_hidden_layers):
+        model.add(keras.layers.Dense(num_neurons, activation='relu'))
+    model.add(keras.layers.Dense(output_neurons, activation='softmax'))
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer=keras.optimizers.SGD(lr=learning_rate),
+                  metrics=['accuracy'])
+    return model
+
+
 def LSTM_predict(X_train, X_test, y_train, y_test):
     model = keras.Sequential()
     model.add(keras.layers.LSTM(
@@ -168,13 +189,17 @@ def LSTM_predict(X_train, X_test, y_train, y_test):
     )
     return model.predict(X_test)
 
+
 def linear_regression_predict(X_train, X_test, y_train, y_test):
     # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
     lin_reg = LinearRegression(normalize=True).fit(X_train, y_train)
     return lin_reg.predict(X_test)
+
+
 def mlp_regression_predict(X_train, X_test, y_train, y_test):
-    mlp_reg = MLPRegressor(activation='logistic', max_iter=5000000, hidden_layer_sizes=()).fit(X_train, y_train)
+    mlp_reg = MLPRegressor(max_iter=5000000, hidden_layer_sizes=()).fit(X_train, y_train)
     return mlp_reg, mlp_reg.predict(X_test)
+
 
 def rise_or_fall_accuracy(y_true, y_pred):
     count = 0
@@ -186,46 +211,97 @@ def rise_or_fall_accuracy(y_true, y_pred):
     return (count / len(y_true)) * 100
 
 
-files = get_stock_subset_num(5)
-for file in files:
-    print(file)
-    window_size = 1
+def doStuff():
+    # %%
+    num_files = 1
+    window_size = 15
+    total_mse = 0
+    total_acc = 0
+    files = get_stock_subset_num(num_files)
 
-    # print_file(file, 10)
-    # X, y = build_data_matrix(file, window_size)
-    X, y = build_np_matrix(file, window_size)
+    for file in files:
+        print(file)
 
-    # normalize
-    # X = normalize(X, axis=1)
+        # print_file(file, 10)
+        # X, y = build_data_matrix(file, window_size)
+        X, y = build_np_matrix(file, window_size)
 
-    # dfx = pd.DataFrame(X)
-    # dfy = pd.DataFrame(y)
+        # normalize
+        # X = normalize(X, axis=1)
 
-    # print_data(X, y)
+        # dfx = pd.DataFrame(X)
+        # dfy = pd.DataFrame(y)
 
-    # print(X.shape)
+        # print_data(X, y)
 
-    # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+        # print(X.shape)
+
+        X_train, X_test, y_train, y_test = [], [], [], []
+        # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+        try:
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+        except:
+            num_files -= 1
+            continue
+
+        y_pred = linear_regression_predict(X_train, X_test, y_train, y_test)
+        # mlp, y_pred = mlp_regression_predict(X_train, X_train, y_train, y_test)
+        # y_pred = LSTM_predict(X_train, X_test, y_train, y_test)
+
+        train_sizes = [1, 100, 500, 2000, 5000, 7654]
+        train_sizes, train_scores, validation_scores = learning_curve(
+            estimator=LinearRegression(),
+            X=X,
+            y=y, cv=5,
+            scoring='neg_mean_squared_error')
+
+        train_scores_mean = -train_scores.mean(axis=1)
+        validation_scores_mean = -validation_scores.mean(axis=1)
+
+        plt.figure()
+        plt.style.use('seaborn')
+        plt.plot(train_sizes, train_scores_mean, label='Training error')
+        plt.plot(train_sizes, validation_scores_mean, label='Validation error')
+        plt.ylabel('MSE', fontsize=14)
+        plt.xlabel('Training set size', fontsize=14)
+        plt.title('Learning curves for a linear regression model', fontsize=18, y=1.03)
+        plt.legend()
+        plt.ylim(0, .01)
+        plt.show()
+        # plt.savefig("test.png")
+
+        # %%
+        for score in train_scores:
+            print(score)
+
+        # print(mlp.n_iter_)
+
+        # m = build_model(2, 50, 1, .01, 50)
+        # keras.utils.plot_model(m)
+
+        # history = m.fit(X_train, y_train)
+
+        # TODO print mean squared error per iteration
+        # report both
+        # average multiple (~10) runs on one stock or take best run
+        # test more on bibl.us.txt
+
+        mse = met.mean_squared_error(y_test, y_pred)
+
+        acc = rise_or_fall_accuracy(y_test, y_pred)
+
+        total_mse += mse
+        total_acc += acc
+
+        print("Mean Squared Error           : " + str(mse))
+        print("Predictive Gain/Loss accuracy: " + str(acc) + "\n")
+
+    avg_mse = total_mse / num_files
+    avg_acc = total_acc / num_files
+
+    print('Average MSE:      ' + str(avg_mse))
+    print('Average Accuracy: ' + str(avg_acc))
 
 
+doStuff()
 
-    y_pred = linear_regression_predict(X_train, X_test, y_train, y_test)
-    # mlp, y_pred = mlp_regression_predict(X_train, X_train, y_train, y_test)
-    # y_pred = LSTM_predict(X_train, X_test, y_train, y_test)
-
-    # print(mlp.n_iter_)
-
-    # TODO print mean squared error per iteration
-    # report both
-    # average multiple (~10) runs on one stock or take best run
-    # test more on bibl.us.txt
-
-    # mse = met.mean_squared_error(y_test, y_pred)
-    mse = met.mean_squared_error(y_test, y_pred)
-
-    # acc = rise_or_fall_accuracy(y_test, y_pred)
-    acc = rise_or_fall_accuracy(y_test, y_pred)
-
-    print("Mean Squared Error           : " + str(mse))
-    print("Predictive Gain/Loss accuracy: " + str(acc) + "\n")
